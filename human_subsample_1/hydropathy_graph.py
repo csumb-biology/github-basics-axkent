e19aab220de4e6f514a07e858e526951d35132a2
axelkent <axkent@csumb.edu>
2019-04-02 19:07:47 -0700

    Take filename and window as arguments, instead of raw input

--- human_subsample_1/hydropathy_graph.py
+++ human_subsample_1/hydropathy_graph.py
@@ -3,6 +3,8 @@
 #Working template of hydropathy score calculation script
 #You need to put in comments for every line
 
+import sys
+
 InFileName = "amino_acid_hydropathy_values.txt"
 InFile = open(InFileName, 'r')
 Data=[]
@@ -18,7 +20,7 @@ for Line in InFile:
 InFile.close()
 
 #window = raw_input("Window size?")
-window=int(window)
+#window=int(window)
 for i in range(len(sys.argv)):
     #if the argument is -i
     if sys.argv[i] == "-i":
@@ -34,8 +36,10 @@ Window = int(Window)
 #convert Threshold to an integer and ensure Threshold variable will be recognized as an integer
 Threshold = int(Threshold)
 
-Value=0
-window_counter=0
+
+
+Value=[]
+window_counter= []
 #InSeqFileName = raw_input("Name of sequence file to analyze?\n")
 InSeqFile = open(InSeqFileName, 'r')
 LineNumber = 0
@@ -53,8 +57,10 @@ for i in range(len(ProtSeq)):
     Value+=Hydropathy[ProtSeq[i]]
     if(i>(window-1) and i<=(len(ProtSeq)-window)):
         Value=Value-Hydropathy[ProtSeq[i-window]]
-        OutString = "%d,%.2f" % (window_counter, Value)
-        OutFile.write(OutString + "\n")
+        window_counter = "%d"
+        Value = "%.2f"
+        #OutString = "%d,%.2f" % (window_counter, Value)
+        #OutFile.write(OutString + "\n")
 
     window_counter+=1
 
